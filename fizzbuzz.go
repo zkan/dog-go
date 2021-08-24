cbb110acd192383d7ff8f0678c80c303d520e8f3
zkan <zkan.cs@gmail.com>
2021-08-24 19:25:20 +0700

    Implement FizzBuzz :bear:

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
+
+func fizzbuzz(number int) string {
+	if number%3 == 0 && number%5 == 0 {
+		return "FizzBuzz"
+	} else if number%3 == 0 {
+		return "Fizz"
+	} else if number%5 == 0 {
+		return "Buzz"
+	} else {
+		return strconv.Itoa(number)
+	}
+}
 
 func main() {
 	fmt.Println("vim-go")
+
+	var x string = "Hello"
+	//x = "Hello"
+	fmt.Println(x)
+
+	y := 10
+	fmt.Println(y)
+
+	var number int = 15
+	result := fizzbuzz(number)
+	fmt.Println(result)
 }
